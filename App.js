53804697e490f70ab97ef721d439b76d376ab3c5
alejandro galaz <alejandrogalaz21@gmail.com>
2022-06-07 23:54:26 -0500

    working whit MusicPlayer screen

--- App.js
+++ App.js
@@ -2,12 +2,10 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import MusicPlayer from './screens/MusicPlayer'
-import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Ionicons name="heart-outline" size={30} />
       <MusicPlayer />
       <StatusBar style="auto" />
     </View>
--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,18 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { StyleSheet, View, SafeAreaView } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const MusicPlayer = () => {
   return (
-    <View style={styles.container}>
-      <Text>React Native Music Player</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainContainer}>
+        <Ionicons name="heart-outline" size={30} />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#555',
+    backgroundColor: '#555'
+  },
+  mainContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   }

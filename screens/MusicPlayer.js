ef6db525e62979fe35d2df7d6e2c64f9b4e14910
alejandro galaz <alejandrogalaz21@gmail.com>
2022-06-08 00:09:49 -0500

    working in botton section

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,12 +1,41 @@
 import React from 'react'
-import { StyleSheet, View, SafeAreaView } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const { width, height } = Dimensions.get('window')
 
 const MusicPlayer = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContainer}>
-        <Ionicons name="heart-outline" size={30} />
+        {/* image */}
+        {/* slider */}
+        {/* music controls */}
+      </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.bottomContainerIconWrapper}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -15,12 +44,24 @@ const MusicPlayer = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#555'
+    backgroundColor: '#222831'
   },
   mainContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  bottomContainer: {
+    width,
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopColor: '#393E46',
+    borderWidth: 1
+  },
+  bottomContainerIconWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%'
   }
 })
 
